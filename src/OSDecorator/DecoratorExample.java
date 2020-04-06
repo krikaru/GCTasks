32004939f88beb70642cb914c4e6e3f2f14975fd
krikaru <lesoleill64@gmail.com>
2020-04-07 00:40:43 +0300

    Change package

--- src/OSDecorator/DecoratorExample.java
+++ src/OSDecorator/DecoratorExample.java
@@ -1,4 +1,4 @@
-package IO.OSDecorator;
+package OSDecorator;
 
 import java.io.*;
 
--- src/OSDecorator/LogOS.java
+++ src/OSDecorator/LogOS.java
@@ -1,4 +1,4 @@
-package IO.OSDecorator;
+package OSDecorator;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
